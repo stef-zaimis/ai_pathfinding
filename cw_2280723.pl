5a841184adbc26690680c48f722388798d454dad
stefanos-zaimis <rd22097@bristol.ac.uk>
2024-11-18 17:00:36 +0000

    Incorporating BFS

--- cw_2280723.pl
+++ cw_2280723.pl
@@ -1,7 +1,8 @@
 % Accomplish a given Task and return the Cost
 solve_task(Task,Cost) :-
     my_agent(A), get_agent_position(A,P),
-    solve_task_dfs(Task,[P],[P|Path]), !,
+    %solve_task_dfs(Task,[P],[P|Path]), !,
+    solve_task_bfs(Task, [[P]], [], Path),
     agent_do_moves(A,Path), length(Path,Cost).
 
 % Calculate the path required to achieve a Task
@@ -10,6 +11,15 @@ solve_task_dfs(Task,[P|Ps],Path) :-
     ;
     map_adjacent(P,Q,empty), \+ member(Q,Ps),
     solve_task_dfs(Task,[Q,P|Ps],Path).
+
+solve_task_bfs(Task, [[Pos|Path]|_], _, RPath) :-
+	achieved(Task, Pos),
+	reverse([Pos|Path], [_|RPath]).
+
+solve_task_bfs(Task, [[Pos|Path]|Rest], Visited, Solution) :-
+	findall([NewPos, Pos|Path], (map_adjacent(Pos, NewPos, empty), \+ member(NewPos, Visited), \+ member([NewPos|_], Rest)), Children),
+	append(Rest, Children, N),
+	solve_task_bfs(Task,N, [Pos|Visited], Solution).
 
 % True if the Task is achieved with the agent at Pos
 achieved(Task,Pos) :- 
