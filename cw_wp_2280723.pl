f0571ad096faa1539f8345c929a428a5cda2ce13
Stefanos <zaimisstefanos@gmail.com>
2024-11-28 17:52:20 +0000

    Better oracle choices

--- cw_wp_2280723.pl
+++ cw_wp_2280723.pl
@@ -24,8 +24,14 @@ find_identity(A) :-
 
 initialise_grid(Oracles, Stations) :-
     ailp_grid_size(N),
-    findall(o(ID), (between(1, N, X), between(1, N, Y), lookup_pos(p(X, Y), o(ID))), Oracles),
+    findall(o(ID)-p(X, Y), (between(1, N, X), between(1, N, Y), lookup_pos(p(X, Y), o(ID))), Oracles),
     findall(c(ID)-p(X, Y), (between(1, N, X), between(1, N, Y), lookup_pos(p(X, Y), c(ID))), Stations).
+
+get_sorted_oracles(Oracles, SortedOracles) :-
+    my_agent(Agent), get_agent_position(Agent, Pos),
+    clustering_scores(Oracles, ClusteringScores),
+    total_scores(Pos, ClusteringScores, Scores),
+    sort_oracles(Scores, SortedOracles).
 
 find_identity_helper(_, [ActorName], _, ActorName).
 find_identity_helper([], CurrentActors, _, ActorName) :-
@@ -34,7 +40,9 @@ find_identity_helper([], CurrentActors, _, ActorName) :-
     ;
         ActorName = unknown
     ).
-find_identity_helper([Oracle|Rest], CurrentActors, Stations, ActorName) :-
+find_identity_helper(Oracles, CurrentActors, Stations, ActorName) :-
+    get_sorted_oracles(Oracles, [Oracle|Rest]), List=[Oracle|Rest],
+    format("Sorted oracles: ~w~n", [List]),
     (
         format("Actor list: ~w ~n", [CurrentActors]),
         ailp_grid_size(N), Threshold is ceil(((N*N)/4)/4),
@@ -43,7 +51,7 @@ find_identity_helper([Oracle|Rest], CurrentActors, Stations, ActorName) :-
         find_identity_helper(Rest, CurrentActors, Stations, ActorName)
     ).
 
-visit_and_query_oracle(Oracle, Actors, Stations, Threshold, NewActors) :-
+visit_and_query_oracle(Oracle-_, Actors, Stations, Threshold, NewActors) :-
     format("Going to new oracle ~n"),
     my_agent(A), get_agent_position(A, Pos), get_agent_energy(A, Energy),
     ailp_grid_size(N), QueryCost is ceil(((N*N)/4)/10),
@@ -75,3 +83,25 @@ visit_and_query_oracle(Oracle, Actors, Stations, Threshold, NewActors) :-
                 format("Included actors: ~w ~n", [NewActors])
             )
     ).
+
+sort_oracles(Scores, SortedOracles) :-
+    sort(Scores, SortedPairs), 
+    pairs_values(SortedPairs, SortedOracles).
+
+pairs_values([], []).
+pairs_values([_-Value|Rest], [Value|Values]) :- pairs_values(Rest, Values).
+
+total_scores(AgentPos, ClusteringScores, Scores) :-
+    W1 is 1, W2 is 1,
+    findall(Score-(Oracle-Pos), (member(ClusterScore-(Oracle-Pos), ClusteringScores), map_distance(AgentPos, Pos, DAgent), Score is W1*DAgent-W2*ClusterScore), Scores).
+
+clustering_scores(Oracles, Scores) :-
+findall(Score-(Oracle-Pos), (member(Oracle-Pos, Oracles), findall(Distance, (member(OtherOracle-OtherPos, Oracles), OtherOracle \= Oracle, map_distance(Pos, OtherPos, Distance), Distance>0), Distances),
+        sum_inverse_distances(Distances, 0, Score)
+    ), Scores).
+
+sum_inverse_distances([], Acc, Acc).
+sum_inverse_distances([D|Ds], Acc, Total) :-
+    InvD is 1/D,
+    NewAcc is Acc+InvD,
+    sum_inverse_distances(Ds, NewAcc, Total).
